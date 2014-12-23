cc1c7c7e2460aa6d893209b5bf5f3d3294d4814d
Trần Ngọc Quân <vnwildman@gmail.com>
2014-12-23 08:55:16 +0700

    Them chuyen cacsh bo dau cua Le Quoc Tuan
    Signed-off-by: Trần Ngọc Quân <vnwildman@gmail.com>

---
+++ src/daucau.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# Lê Quốc Tuấn <mr.lequoctuan@gmail.com>, 2014.
+# Trần Ngọc Quân <vnwildman@gmail.com>, 2014.
+
+import re
+from sys import argv
+
+MAPS = {
+    'uỳ': 'ùy',
+    'uý': 'úy',
+    'uỷ': 'ủy',
+    'uỹ': 'ũy',
+    'uỵ': 'ụy',
+    'oà': 'òa',
+    'oá': 'óa',
+    'oả': 'ỏa',
+    'oã': 'õa',
+    'oạ': 'ọa',
+    'oè': 'òe',
+    'oé': 'óe',
+    'oẻ': 'ỏe',
+    'oẽ': 'õe',
+    'oẹ': 'ọe'
+}
+maps1 = {k.upper(): v.upper() for k, v in MAPS.items()}
+maps2 = {k.capitalize(): v.capitalize() for k, v in MAPS.items()}
+maps3 = {k[0]+k[1].upper(): v[0]+v[1].upper() for k, v in MAPS.items()}
+MAPS.update(maps1)
+MAPS.update(maps2)
+MAPS.update(maps3)
+
+PAT_DAUKIEUMOI = re.compile(r'(?<!q)(%s)(?!\w)' % '|'.join(MAPS.keys()), re.I)
+
+def sub_kieucu(match):
+    s = match.group(1)
+    return MAPS[s]
+
+def daukieucu(input_str):
+    return PAT_DAUKIEUMOI.sub(sub_kieucu, input_str)
+
+if __name__ == '__main__':
+#    a = 'hoả Hoả HOẢ HOả quả Quả huỷ huỳ huý quý huỵnh huỳnh quýnh tuýn hoá hoản'
+#    b = daukieucu(a)
+#    print(a)
+#    print(b)
+    script, filename = argv
+    txt = open(filename)
+    buffer = txt.read()
+    print(daukieucu(buffer))
+
